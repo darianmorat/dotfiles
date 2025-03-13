5cd78b782beacbf03ffac1a11551be0d4eaf26c4
darianmorat <yojhandariantoledomora@gmail.com>
2025-03-12 21:26:32 -0500

    fix(wezterm): add toggle theme keymap

--- config/terminal/wezterm.lua
+++ config/terminal/wezterm.lua
@@ -92,134 +92,132 @@ config.keys = {
    { key = "j", mods = "LEADER", action = wezterm.action.SendString "\x15vifm\n" },
    { key = "k", mods = "LEADER", action = wezterm.action.SendString "\x15nvim\n" },
 
+   -- Toogle theme
+   { key = "Tab", mods = "CTRL", action = wezterm.action.EmitEvent("toggle-theme") },
+
    -- Disable defaults
    { key = "Enter", mods = "ALT", action = wezterm.action.DisableDefaultAssignment },
    { key = "Tab", mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
+
 }
 
 -- --------------------------------------------------------------------------------------
--- DARK COLORS
+-- WEZTERM THEMES
 -- --------------------------------------------------------------------------------------
-config.force_reverse_video_cursor = true
+local theme_file = "//wsl$/Ubuntu/home/darianmorat/.theme"
 
-config.foreground_text_hsb = {
-   saturation = 1.03,
-   brightness = 1.2,
-}
+local themes = {
+   dark = {
+      foreground = "#CDC5B8",
+      background = "#1E1E1E",
+      cursor_fg = "#1E1E1E",
+      cursor_bg = "#C6C5C5",
+      cursor_border = "#C6C5C5",
+      selection_fg = "#CDC5B8",
+      selection_bg = "#303030",
+      split = "#4F4F4F",
+      scrollbar_thumb = "#4F4F4F",
+      copy_mode_active_highlight_bg = { Color = "#1E1E1E" },
+      copy_mode_active_highlight_fg = { Color = "#CDC5B8" },
+      copy_mode_inactive_highlight_bg = { Color = "#76B568" },
+      copy_mode_inactive_highlight_fg = { Color = "#CDC5B8" },
+      quick_select_label_bg = { Color = "#D19F66" },
+      quick_select_label_fg = { Color = "#CDC5B8" },
+      quick_select_match_bg = { Color = "#5B98C9" },
+      quick_select_match_fg = { Color = "#CDC5B8" },
 
-config.colors = {
-   foreground = "#CDC5B8",
-   background = "#1E1E1E",
-   cursor_fg = "#1E1E1E",
-   cursor_bg = "#C6C5C5",
-   cursor_border = "#C6C5C5",
-
-   selection_fg = "#CDC5B8",
-   selection_bg = "#303030",
-
-   split = "#4F4F4F",
-   scrollbar_thumb = "#4F4F4F",
-
-   ansi = {
-      "#4F4F4F", -- black
-      "#DB6A6A", -- red
-      "#76B568", -- green
-      "#D19F66", -- yellow
-      "#5B98C9", -- blue
-      "#CD60B9", -- purple
-      "#00AA9C", -- cyan
-      "#CDC5B8", -- white
+      ansi = {
+         "#4F4F4F", -- black
+         "#DB6A6A", -- red
+         "#76B568", -- green
+         "#D19F66", -- yellow
+         "#5B98C9", -- blue
+         "#CD60B9", -- purple
+         "#00AA9C", -- cyan
+         "#CDC5B8", -- white
+      },
+      brights = {
+         "#4F4F4F", -- brightBlack
+         "#DB6A6A", -- brightRed
+         "#76B568", -- brightGreen
+         "#D19F66", -- brightYellow
+         "#5B98C9", -- brightBlue
+         "#CD60B9", -- brightPurple
+         "#00AA9C", -- brightCyan
+         "#CDC5B8", -- brightWhite
+      }
    },
-   brights = {
-      "#4F4F4F", -- brightBlack
-      "#DB6A6A", -- brightRed
-      "#76B568", -- brightGreen
-      "#D19F66", -- brightYellow
-      "#5B98C9", -- brightBlue
-      "#CD60B9", -- brightPurple
-      "#00AA9C", -- brightCyan
-      "#CDC5B8", -- brightWhite
-   },
 
-   indexed = {},
-   copy_mode_active_highlight_bg = { Color = "#1E1E1E" },
-   copy_mode_active_highlight_fg = { Color = "#CDC5B8" },
-   copy_mode_inactive_highlight_bg = { Color = "#76B568" },
-   copy_mode_inactive_highlight_fg = { Color = "#CDC5B8" },
+   light = {
+      foreground = "#111111",
+      background = "#f9f6e4",
+      cursor_fg = "#eae7e1",
+      cursor_bg = "#404040",
+      cursor_border = "#404040",
+      selection_fg = "#111111",
+      selection_bg = "#b0b0a6",
+      split = "#4F4F4F",
+      scrollbar_thumb = "#4F4F4F",
+      copy_mode_active_highlight_bg = { Color = "#f9f6e4" },
+      copy_mode_active_highlight_fg = { Color = "#111111" },
+      copy_mode_inactive_highlight_bg = { Color = "#006c00" },
+      copy_mode_inactive_highlight_fg = { Color = "#111111" },
+      quick_select_label_bg = { Color = "#ae5f05" },
+      quick_select_label_fg = { Color = "#111111" },
+      quick_select_match_bg = { Color = "#1e5a8b" },
+      quick_select_match_fg = { Color = "#111111" },
 
-   quick_select_label_bg = { Color = "#D19F66" },
-   quick_select_label_fg = { Color = "#CDC5B8" },
-   quick_select_match_bg = { Color = "#5B98C9" },
-   quick_select_match_fg = { Color = "#CDC5B8" },
+      ansi = {
+         "#1B1B1B", -- black
+         "#9f0202", -- red
+         "#006c00", -- green
+         "#ae5f05", -- yellow
+         "#1e5a8b", -- blue
+         "#910e79", -- purple
+         "#008b7f", -- cyan
+         "#111111", -- white
+      },
+      brights = {
+         "#111111", -- brightBlack
+         "#9f0202", -- brightRed
+         "#006c00", -- brightGreen
+         "#ae5f05", -- brightYellow
+         "#1e5a8b", -- brightBlue
+         "#910e79", -- brightPurple
+         "#008b7f", -- brightCyan
+         "#111111", -- brightWhite
+      }
+   }
 }
 
--- --------------------------------------------------------------------------------------
--- LIGHT COLORS
--- --------------------------------------------------------------------------------------
--- config.colors = {
---    foreground = "#111111",
---    background = "#f9f6e4",
---    cursor_fg = "#eae7e1",
---    cursor_bg = "#404040",
---    cursor_border = "#404040",
---
---    selection_fg = "#111111",
---    selection_bg = "#b0b0a6",
---
---    split = "#4F4F4F",
---    scrollbar_thumb = "#4F4F4F",
---
---    ansi = {
---       "#1B1B1B", -- black
---       "#9f0202", -- red
---       "#006c00", -- green
---       "#ae5f05", -- yellow
---       "#1e5a8b", -- blue
---       "#910e79", -- purple
---       "#008b7f", -- cyan
---       "#111111", -- white
---    },
---    brights = {
---       "#111111", -- brightBlack
---       "#9f0202", -- brightRed
---       "#006c00", -- brightGreen
---       "#ae5f05", -- brightYellow
---       "#1e5a8b", -- brightBlue
---       "#910e79", -- brightPurple
---       "#008b7f", -- brightCyan
---       "#111111", -- brightWhite
---    },
---
---    indexed = {},
---    copy_mode_active_highlight_bg = { Color = "#f9f6e4" },
---    copy_mode_active_highlight_fg = { Color = "#111111" },
---    copy_mode_inactive_highlight_bg = { Color = "#006c00" },
---    copy_mode_inactive_highlight_fg = { Color = "#111111" },
---
---    quick_select_label_bg = { Color = "#ae5f05" },
---    quick_select_label_fg = { Color = "#111111" },
---    quick_select_match_bg = { Color = "#1e5a8b" },
---    quick_select_match_fg = { Color = "#111111" },
--- }
+local function read_theme()
+   local file = io.open(theme_file, "r")
+   local theme = file and file:read("*l"):match("%S+") or "dark"
+   if file then file:close() end
+   return themes[theme] and theme or "dark"
+end
+
+local function save_theme(theme)
+   local file = io.open(theme_file, "w")
+   if file then file:write(theme) file:close() end
+end
+
+local current_theme = read_theme()
+config.colors = themes[current_theme]
+
+wezterm.on("toggle-theme", function(window)
+   current_theme = (current_theme == "dark") and "light" or "dark"
+   window:set_config_overrides({ 
+      colors = themes[current_theme],
+      foreground_text_hsb = (current_theme == "dark") and {
+         saturation = 1.03,
+         brightness = 1.2
+      } or nil
+   })
+   save_theme(current_theme)
+end)
 
 -- --------------------------------------------------------------------------------------
--- BACKGROUND
+-- ENDING
 -- --------------------------------------------------------------------------------------
--- config.background = {
---    {
---       source = {
---          File = "d:/apps/wezterm/images/32.jpg",
---       },
---       opacity = 1.0,
---    },
---    {
---       source = {
---          Color = "#202020",
---       },
---       width = "400",
---       height = "1080",
---       opacity = 0.94,
---    }
--- }
-
 return config
