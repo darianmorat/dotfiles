d592aff7cd8d76a403aac676343f7cd4d18aee7a
Darian Toledo <80854633+darianmorat@users.noreply.github.com>
2026-04-27 13:55:20 -0500

    fix(shell): keep defaults and add notes

--- root/.config/wezterm/wezterm.lua
+++ root/.config/wezterm/wezterm.lua
@@ -9,19 +9,6 @@ if wezterm.config_builder then
    config = wezterm.config_builder()
 end
 
-config.window_background_opacity = 1
-config.skip_close_confirmation_for_processes_named = {
-   "bash",
-   "sh",
-   "zsh",
-   "fish",
-   "tmux",
-   "nu",
-   "cmd.exe",
-   "pwsh.exe",
-   "powershell.exe",
-}
-
 config.font = wezterm.font({
    family = "JetBrainsMono Nerd Font Mono",
    weight = "Medium",
