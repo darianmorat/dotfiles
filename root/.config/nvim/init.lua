15c04018ee5e595d713b2b764afea6d34d933fe9
Darian Toledo <80854633+darianmorat@users.noreply.github.com>
2026-07-17 09:03:33 -0500

    chore(nvim): formatting for empty lines

--- root/.config/nvim/init.lua
+++ root/.config/nvim/init.lua
@@ -589,12 +589,10 @@ require("conform").setup({
    formatters_by_ft = {
       lua = { "stylua" },
       python = { "black" },
-
       javascript = prettier,
       javascriptreact = prettier,
       typescript = prettier,
       typescriptreact = prettier,
-
       html = prettier,
       css = prettier,
       json = prettier,
