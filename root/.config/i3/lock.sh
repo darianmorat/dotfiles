4379409e0f4c0e627b36855ccc394880b3b6e248
Darian Toledo <80854633+darianmorat@users.noreply.github.com>
2026-08-11 20:15:41 -0500

    fix(i3): update screen idle config

--- root/.config/i3/lock.sh
+++ root/.config/i3/lock.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 i3lock --nofork -c 000000 &
 LOCK_PID=$!
-(sleep 20 && kill -0 $LOCK_PID 2>/dev/null && xset dpms force off) &
+(sleep 10 && kill -0 $LOCK_PID 2>/dev/null && xset dpms force off) &
 wait $LOCK_PID
-xset dpms 0 0 0
