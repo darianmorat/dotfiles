752c78205c4bcfc8cfcd3178efce9ea0969cfb0a
Darian Toledo <dariantoledo@duck.com>
2026-09-07 18:21:32 -0500

    fix(nvim): update gitsigns sign glyphs

--- root/.config/nvim/init.lua
+++ root/.config/nvim/init.lua
@@ -371,14 +371,20 @@ vim.keymap.set("n", "<leader>fW", fzf_vertical("grep_cWORD"))
 
 require("gitsigns").setup({
    signs = {
-      add = { text = "❘" },
-      change = { text = "❘" },
+      add = { text = "│" },
+      change = { text = "│" },
       delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+      untracked = { text = "┆" },
    },
    signs_staged = {
-      add = { text = "❘" },
-      change = { text = "❘" },
+      add = { text = "│" },
+      change = { text = "│" },
       delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+      untracked = { text = "┆" },
    },
 
    on_attach = function(bufnr)
